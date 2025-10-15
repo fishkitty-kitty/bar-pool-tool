7372afec27248de0cba4b74797e0fa2944c76f7b
Maya <portmdel@gmail.com>
2025-10-15 14:40:29 -0500

    Renamed each field in formset to appropriate rating categories

--- poolproject/pooldata/forms.py
+++ poolproject/pooldata/forms.py
@@ -1,10 +1,13 @@
 from django import forms
-from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class RatingForm(forms.Form):
     title = forms.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
-
-
-# class RatingForm(models.Model):
-#     overs = models.IntegerField(default=0, choices=((i,i) for i in range(0,11)))
+    categories = ['Staff', 'Ambience',	'Patrons',	'Tables Count',	'Table Quality', 'Gear Quality', 'Food', 'Drink', 'Game Cost']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for category in self.categories:
+            self.fields[category] = forms.IntegerField(
+                label= category,
+                validators=[MinValueValidator(0), MaxValueValidator(10)]
+            )
--- poolproject/pooldata/views.py
+++ poolproject/pooldata/views.py
@@ -3,26 +3,8 @@ from django.shortcuts import render
 from .forms import RatingForm
 from django.forms import formset_factory
 
-from .models import Post
-from django.utils import timezone
-
-# def input_name(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         if name:
-#             return HttpResponse(f"Hello, {name}! You've successfully entered your name.")
-#         else:
-#             return HttpResponse("Please enter a valid name.")
-#
-#     return render(request, 'pooldata/post_list.html')
-# def return_data(request):
-#     return HttpResponse('')
-
-# def test_view(request):
-#     return HttpResponse("This is a test view!")
-
 def formset_view(request):
-    RatingFormSet = formset_factory(RatingForm, extra = 5)
+    RatingFormSet = formset_factory(RatingForm)
     if request.method == 'POST':
         formset = RatingFormSet(request.POST)
         if formset.is_valid():
@@ -30,4 +12,5 @@ def formset_view(request):
                 print(form.cleaned_data)
     else:
         formset = RatingFormSet()
+
     return render(request, "pooldata/home.html", {'formset': formset})
